91d8ba12a754c4b6786f48741b17bd60a5122d07
Daka-fish <yamatohidaka24@gmail.com>
2024-05-02 21:47:30 +0900

    Deckクラスの修正

--- src/main/java/net/tv/twitch/chrono_fish/ito/Deck.java
+++ src/main/java/net/tv/twitch/chrono_fish/ito/Deck.java
@@ -3,6 +3,7 @@ package net.tv.twitch.chrono_fish.ito;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -11,4 +12,37 @@ public class Deck {
     List<Card> deck = new ArrayList<>();
     List<Card> field = new ArrayList<>();
     HashMap<Player, Card> map = new HashMap<>();
+
+    Deck(){
+        setDeck();
+        shuffleDeck();
+    }
+
+    public void setDeck() {
+        for(int i=1; i<=100; i++){
+            this.deck.add(new Card(i));
+        }
+    }
+
+    public void shuffleDeck(){
+        Collections.shuffle(deck);
+    }
+
+    public Card drawCard(){
+        Card fstCard = this.deck.get(0);
+        this.deck.remove(0);
+        return fstCard;
+    }
+
+    public void putField(Card card){
+        this.field.add(card);
+    }
+
+    public List getField(){
+        return field;
+    }
+
+    public void putMap(Player player, Card card){
+        this.map.put(player,card);
+    }
 }
