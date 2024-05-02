edefd858281856b374f2ef6e1bd317a38cda8fba
Daka-fish <yamatohidaka24@gmail.com>
2024-05-02 22:20:01 +0900

    BossBarManagerの追加

---
+++ src/main/java/net/tv/twitch/chrono_fish/ito/BossBarManager.java
@@ -0,0 +1,25 @@
+package net.tv.twitch.chrono_fish.ito;
+
+import org.bukkit.Bukkit;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.boss.BossBar;
+import org.bukkit.entity.Player;
+
+public class BossBarManager {
+    static BossBar bossBar;
+
+    BossBarManager(){
+        bossBar = Bukkit.createBossBar("ito", BarColor.PINK, BarStyle.SEGMENTED_20);
+    }
+
+    public static BossBar getBossBar(){
+        return bossBar;
+    }
+
+    public void addAllPlayer(){
+        for(Player player : Bukkit.getOnlinePlayers()){
+            bossBar.addPlayer(player);
+        }
+    }
+}
