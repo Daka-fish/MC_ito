e6e3e429586c88c9a6b3066cf40c346e8b26727e
Daka-fish <yamatohidaka24@gmail.com>
2024-05-02 20:46:22 +0900

    Cardの追加

---
+++ src/main/java/net/tv/twitch/chrono_fish/ito/Card.java
@@ -0,0 +1,12 @@
+package net.tv.twitch.chrono_fish.ito;
+
+public class Card {
+    int number;
+    Card(int i){
+        this.number = i;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
