d9d672e0932efd720bad82c3339db07daa31d3b7
Daka-fish <yamatohidaka24@gmail.com>
2024-05-03 17:30:15 +0900

    showField関数の追加

--- src/main/java/net/tv/twitch/chrono_fish/ito/DeckManager.java
+++ src/main/java/net/tv/twitch/chrono_fish/ito/DeckManager.java
@@ -1,11 +1,13 @@
 package net.tv.twitch.chrono_fish.ito;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DeckManager {
     int count;
@@ -42,11 +44,21 @@ public class DeckManager {
         this.field.add(card);
     }
 
-    public List getField(){
+    public List<Card> getField(){
         return field;
     }
 
     public void putMap(Player player, Card card){
         this.map.put(player,card);
+    }
+
+    public void showField(){
+        StringBuilder s = new StringBuilder();
+        s.append("==========================\n");
+        s.append("プレイヤー名"+"\t"+"(数字)\n");
+        for(Map.Entry<Player, Card> entry : map.entrySet()){
+            s.append(entry.getKey().getName() + "\t" + "("+entry.getValue().getNumber()+")\n");
+        }
+        Bukkit.broadcastMessage(s.toString());
     }
 }
