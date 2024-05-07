bc23989197c2e38600ac8c16eb965fbb112f6341
Daka-fish <yamatohidaka24@gmail.com>
2024-05-08 08:52:22 +0900

    ItoEventの追加

---
+++ src/main/java/net/tv/twitch/chrono_fish/ito/ItoEvent.java
@@ -0,0 +1,13 @@
+package net.tv.twitch.chrono_fish.ito;
+
+import org.bukkit.boss.BossBar;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class ItoEvent implements Listener {
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e){
+    }
+}
