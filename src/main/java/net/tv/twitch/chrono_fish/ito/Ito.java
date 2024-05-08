f219510a6b0e1aca3c363c90b7cf2dd377dca5a7
Daka-fish <yamatohidaka24@gmail.com>
2024-05-08 09:36:44 +0900

    ItoEventの修正

--- src/main/java/net/tv/twitch/chrono_fish/ito/Ito.java
+++ src/main/java/net/tv/twitch/chrono_fish/ito/Ito.java
@@ -1,6 +1,7 @@
 package net.tv.twitch.chrono_fish.ito;
 
 import net.tv.twitch.chrono_fish.ito.CommandPack.ItoCommands;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Ito extends JavaPlugin {
@@ -8,6 +9,7 @@ public final class Ito extends JavaPlugin {
     @Override
     public void onEnable() {
         registerCommands();
+        Bukkit.getPluginManager().registerEvents(new ItoEvent(),this);
     }
 
     public void registerCommands(){
