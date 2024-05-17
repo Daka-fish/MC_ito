b8657d88f880dc083326a0fb5897c6a44d57c707
Daka-fish <yamatohidaka24@gmail.com>
2024-05-17 12:06:46 +0900

    startコマンドの修正

--- src/main/java/net/tv/twitch/chrono_fish/ito/CommandPack/CommandManager.java
+++ src/main/java/net/tv/twitch/chrono_fish/ito/CommandPack/CommandManager.java
@@ -28,6 +28,7 @@ public class CommandManager {
                     itogame.startGame();
                     itogame.reloadBossBar();
                     itogame.broadcastMessage("テーマは"+itogame.getTheme()+"です\n");
+                    itogame.broadcastMessage("それぞれのプレイヤーが紙を捨てることで\n数字の宣言となります");
                     itogame.dealCard();
                 }
                 break;
